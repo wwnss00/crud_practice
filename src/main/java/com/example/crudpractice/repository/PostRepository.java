89f72c26b9fe17de085f3e6a757e93dc91ed737d
wwnss00 <wns208500@gmail.com>
2025-09-16 15:50:49 +0900

    feat: PostRepository 레포지토리 어노테이션 추가

--- src/main/java/com/example/crudpractice/repository/PostRepository.java
+++ src/main/java/com/example/crudpractice/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.example.crudpractice.repository;
 
 import com.example.crudpractice.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
