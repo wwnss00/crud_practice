59edd282f8007da3990a157a3fb6acf69ed2b06e
wwnss00 <wns208500@gmail.com>
2025-09-16 15:55:17 +0900

    feat: PostService 생성&저장코드 추가

--- src/main/java/com/example/crudpractice/service/PostService.java
+++ src/main/java/com/example/crudpractice/service/PostService.java
@@ -1,7 +1,21 @@
 package com.example.crudpractice.service;
 
+import com.example.crudpractice.entity.Post;
+import com.example.crudpractice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void save(Post post) {
+        postRepository.save(post);
+    }
+
+
 }
