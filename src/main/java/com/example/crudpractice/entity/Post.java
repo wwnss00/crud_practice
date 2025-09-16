fa5190d9d8d0c392601daf3e280dbf6ece152daf
wwnss00 <wns208500@gmail.com>
2025-09-16 15:34:54 +0900

    feat: post 엔티티 추가

---
+++ src/main/java/com/example/crudpractice/entity/Post.java
@@ -0,0 +1,17 @@
+package com.example.crudpractice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Post {
+
+    @Id
+    private Long id;
+    private String title;
+    private String content;
+
+}
