82ad6e5d739a2ea00024bc3545dd3ee641b1f3da
wwnss00 <wns208500@gmail.com>
2025-09-16 15:39:42 +0900

    feat: post엔티티 id 기본설정

--- src/main/java/com/example/crudpractice/entity/Post.java
+++ src/main/java/com/example/crudpractice/entity/Post.java
@@ -1,6 +1,8 @@
 package com.example.crudpractice.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +12,7 @@ import lombok.Setter;
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
