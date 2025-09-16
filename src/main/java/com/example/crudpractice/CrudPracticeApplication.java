451d60b56e7c41ee1624c6a811630f3eb8431e3a
wwnss00 <wns208500@gmail.com>
2025-09-16 15:06:06 +0900

    refactor: 패키지 구조 변경

--- src/main/java/com/example/crudpractice/CrudPracticeApplication.java
+++ src/main/java/com/example/crudpractice/CrudPracticeApplication.java
@@ -1,4 +1,4 @@
-package practice.crud_practice;
+package com.example.crudpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
