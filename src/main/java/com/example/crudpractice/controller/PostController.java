dee5fb30c1ea461f4a347cc3c31a25830981732d
wwnss00 <wns208500@gmail.com>
2025-09-16 17:54:06 +0900

    feat: 새 글 작성 페이지 불러오기 코드 추가

--- src/main/java/com/example/crudpractice/controller/PostController.java
+++ src/main/java/com/example/crudpractice/controller/PostController.java
@@ -1,2 +1,28 @@
-package com.example.crudpractice.controller;public class PostController {
+package com.example.crudpractice.controller;
+
+
+import com.example.crudpractice.entity.Post;
+import com.example.crudpractice.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/")
+    public String home() {
+        return "post/index";
+    }
+
+
+    @GetMapping("/posts/save")
+    public String saveForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "post/createPostForm";
+    }
 }
